bdf98c4ad293548495c9442c72ffb074ebaf1d4f
quoioln <quoipro94@gmail.com>
2015-08-09 09:57:05 +0700

    giao dien cong van

---
+++ source/danhmuc/js/cong-van.js
@@ -0,0 +1,15 @@
+function showForm(formId1, formId2, check){
+    if (check)
+        document.getElementById(formId2).style.display="block";
+    else 
+        document.getElementById(formId2).style.display="none";
+    var f = document.getElementById(formId1), s, opacity;
+    s = f.style;
+    opacity = check? '10' : '100';
+    s.opacity = s.MozOpacity = s.KhtmlOpacity = opacity/100;
+    s.filter = 'alpha(opacity='+opacity+')';
+    for(var i=0; i<f.length; i++) f[i].disabled = check;
+}
+function confirmDelete(){
+    return confirm('Bạn có chắc xóa');
+}
