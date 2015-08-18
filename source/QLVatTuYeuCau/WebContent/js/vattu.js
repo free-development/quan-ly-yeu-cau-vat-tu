a3c11336cdfa0094d4ae5bbdae82c1275541a681
camnhung <camnhung864271@gmail.com>
2015-08-18 19:38:13 +0700

    update vattu

---
+++ source/QLVatTuYeuCau/WebContent/js/vattu.js
@@ -0,0 +1,160 @@
+		function showForm(formId, check){
+			if (check)
+				document.getElementById(formId).style.display="block";
+			else document.getElementById(formId).style.display="none";
+			var f = document.getElementById('main-form'), s, opacity;
+			s = f.style;
+			opacity = check? '10' : '100';
+			s.opacity = s.MozOpacity = s.KhtmlOpacity = opacity/100;
+			s.filter = 'alpha(opacity='+opacity+')';
+			for(var i=0; i<f.length; i++) f[i].disabled = check;
+		}	
+		function addVattu() {
+ 			vtMa = $('#add-form input:text[name=vtMa]').val();
+ 			vtTen = $('#add-form input:text[name=vtTen]').val();
+ 			dvt = $('#add-form select[name=dvt]').val();
+ 			if(vtMa == '') {
+ 				$('#requireVtMa').html('Vui long nhap ma vat tu');
+ 			}
+ 			else {
+ 			//var chiTiet = $('#add-form button[name=chiTiet]').val();
+// 			alert(dvt);
+// 			alert(chiTie
+ 			$.ajax({
+ 				url: "/QLVatTuYeuCau/addVattu.html",	
+			  	type: "GET",
+ 			  	dateType: "JSON",
+ 			  	data: { "vtMa": vtMa, "vtTen": vtTen, "dvt": dvt},
+ 			  	contentType: 'application/json',
+ 			    mimeType: 'application/json',
+			  	
+ 			  	success: function(result) {
+			  		if(result == "success")
+	 				{
+					$('#view-table-vat-tu table tr:first').after('<tr><td class=\"left-column\"><input type=\"checkbox\" name=\"vtMa\" value=\"' +vtMa + '\"</td><td class=\"col\">'+ vtMa +'</td><td class=\"col\">' + vtTen+'</td></tr>');
+			  		$('#add-form input:text[name=vtMa]').val('');
+					$('#add-form input:text[name=vtTen]').val('');
+					$('#add-form select[name=dvt]').val('');
+			  		showForm("add-form", false);	
+			  		alert("Vai trò "+vtMa + " đã được thêm ");	
+				}
+		  		else{
+		  			alert("Vai trò "+vtMa + " đã tồn tại ");
+		  		}
+			  	
+ 			  	}
+ 			});
+ 			}
+ 		}
+		function preEditVattu(formId, check){
+			vtMa = $('input:checkbox[name=vtMa]:checked').val();
+			//alert(vtMa);
+				$.ajax({
+					url: "/QLVatTuYeuCau/preEditVattu.html",
+					type: "GET",
+					dataType: "JSON",
+					data: {"vtMa": vtMa},
+					contentType: "application/json",
+					mimeType: "application/json",
+					
+					success: function(vt){
+						
+						$('input:text[name=vtMaUpdate]').val(vt.vtMa);
+					  	$('input:text[name=vtTenUpdate]').val(vt.vtTen);
+						$('select[name=dvtUpdate]').val(vt.dvt);
+					  	
+					  	showForm(formId, check);
+					}
+					
+				});
+		}
+
+		function confirmUpdateVattu(){
+			var vtMaUpdate = $('input:text[name=vtMaUpdate]').val();
+			var vtTenUpdate = $('input:text[name=vtTenUpdate]').val();
+			var dvtUpdate = $('select[name=dvtUpdate]').val();
+			if (confirm('Bạn có chắc thay đổi vật tư có mã ' + vtMaUpdate))
+				updateVattu(vtMaUpdate, vtTenUpdate, dvtUpdate);
+		}
+ 		function updateVattu(vtMaUpdate, vtTenUpdate, dvtUpdate){
+ 			$.ajax({
+				url: "/QLVatTuYeuCau/updateVattu.html",	
+			  	type: "GET",
+			  	dateType: "JSON",
+			  	data: { "vtMaUpdate": vtMaUpdate, "vtTenUpdate": vtTenUpdate},
+			  	contentType: 'application/json',
+			    mimeType: 'application/json',
+				  	
+				  	success: function(vt) {
+				  		$('table tr').has('input[name="vtMa"]:checked').remove();
+				  		$('#view-table table tr:first').after('<tr><td class=\"left-column\"><input type=\"checkbox\" name=\"vtMa\" value=\"' +vtMaUpdate + '\"</td><td class=\"col\">'+ vtMaUpdate +'</td><td class=\"col\">' + vtTenUpdate+'</td></tr>');
+				  		$('input:text[name=vtMaUpdate]').val('');
+				 
+						vtTenUpdate = $('input:text[name=vtTenUpdate]').val('');
+						dvtUpdate = $('select[name=dvtUpdate]').val('');
+				  		showForm("update-form", false);	
+				  	}
+				});
+ 	}
+ 	function confirmDelete(){
+ 		vtMa = $('input:checkbox[name=vtMa]:checked').val();
+ 		if (confirm('Bạn có chắc xóa ' + vtMa))
+ 			deleteVattu(vtMa);
+ 	}
+	
+  	 function deleteVattu(vtMa) {
+		 
+ 		$.ajax({
+ 			url: "/QLVatTuYeuCau/deleteVattu.html",	
+ 		  	type: "GET",
+ 		  	dateType: "JSON",
+ 		  	data: { "vtMa": vtMa},
+ 		  	contentType: 'application/json',
+ 		    mimeType: 'application/json',
+ 		    
+ 		    
+ 		  	success: function() {
+ 		  		alert(vtMa + "da bi xoa");
+				$('#view-table-vat-tu table tr').has('input[name="vtMa"]:checked').remove();
+ 		    } 
+ 		});  
+ 	}
+  	function showChiTiet(formId,check, vtMa){  
+  		$.ajax({
+  			url: "/QLVatTuYeuCau/showCTVatTu.html",
+			type: "GET",
+		  	dateType: "JSON",
+		  	data: {"vtMa": vtMa},
+		  	contentType: 'application/json',
+		    mimeType: 'application/json',
+			
+			success: function(listCTVatTu){
+				alert("ok");
+
+//				for(i = 0;i < listCTVatTu.length; i++ ) { 
+//
+//				$('#view-table table tr:first').after("<tr><td class=\"left-column\"><input type=\"checkbox\" name=\"vtMa\" value=\""
+//						+ listCTVatTu[i].VatTu.vtMa + "\" class=\"checkbox\"></td>"
+//				+"<td class=\"col\">" +listCTVatTu[i].vatTu.vtMa+ "</td>"
+//				+"<td class=\"col\">" +listCTVatTu[i].vatTu.vtTen+ "</td>"
+//				+"<td class=\"col\">" +listCTVatTu[i].noiSanXuat.nsxTen+ "</td>"
+//				+"<td class=\"col\">" +listCTVatTu[i].chatLuong.clTen+ "</td>"
+//				+"<td class=\"col\">" +listCTVatTu[i].vatTu.dvt+ "</td>"
+//				+"<td class=\"col\">" +listCTVatTu[i].dinhMuc+ "</td>"
+//				+"<td class=\"col\">" +listCTVatTu[i].soLuongTon+ "</td></tr>");
+//				}
+			  	showForm(formId, check);
+			}
+  		});
+  	}
+  	function clickMe() {
+  		
+  		vtMa = $('label[name=text]').val();
+  		alert(vtMa);
+// 		if (confirm('Bạn có chắc xóa ' + vtMa))
+// 			deleteVattu(vtMa);
+  	} 
+  	function changeVtMa(){
+  		$('#requireVtMa').html('');
+  		$('#add-form input:text[name=vtMa]').focus();
+ 	} 	
---
+++ source/QLVatTuYeuCau/src/controller/VattuController.java
@@ -0,0 +1,116 @@
+package controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.CTVatTu;
+import model.ChatLuong;
+import model.NoiSanXuat;
+import model.VaiTro;
+import model.VatTu;
+
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import util.JSonUtil;
+import dao.VatTuDAO;
+import dao.CTVatTuDAO;
+
+@Controller
+public class VattuController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   @RequestMapping("/manageVattu")
+	protected ModelAndView manageCtvt(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		VatTuDAO vatTuDAO = new VatTuDAO();
+		
+		String action = request.getParameter("action");
+		if("addVatTu".equalsIgnoreCase(action)) {
+			
+			String vtMa = request.getParameter("vtMa");
+			String vtTen = request.getParameter("vtTen");
+			String dvt = request.getParameter("dvt");
+
+			if(new VatTuDAO().getVatTu(vtMa) != null){
+				request.setAttribute("error", "Vật tư đã tồn tại");
+				System.out.println("Vat tu da ton tai");
+				return new ModelAndView("danh-muc-vat-tu");
+			}
+			else{
+				vatTuDAO.addVatTu(new VatTu(vtMa,vtTen,dvt));
+				ArrayList<VatTu> vatTuList =  (ArrayList<VatTu>) new VatTuDAO().getAllVatTu();
+				return new ModelAndView("danh-muc-vat-tu", "vatTuList", vatTuList);
+			}
+			
+		}
+		if("deleteVatTu".equalsIgnoreCase(action)) {
+			String[] vtMaList = request.getParameterValues("vtMa");
+			for(String s : vtMaList) {
+				
+					vatTuDAO.deleteVatTu(vatTuDAO.getVatTu(s));
+			}
+			ArrayList<VatTu> vatTuList =  (ArrayList<VatTu>) vatTuDAO.getAllVatTu();
+			return new ModelAndView("danh-muc-vat-tu", "vatTuList", vatTuList);
+
+		}
+		if("manageVattu".equalsIgnoreCase(action)) {
+			ArrayList<VatTu> vatTuList =  (ArrayList<VatTu>) new VatTuDAO().getAllVatTu();
+			return new ModelAndView("danh-muc-vat-tu", "vatTuList", vatTuList);
+		}
+		return new ModelAndView("login");
+	}
+   @RequestMapping(value="/preEditVattu", method=RequestMethod.GET,
+			produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	 public @ResponseBody String preEditVattu(@RequestParam("vtMa") String vtMa) {
+			System.out.println("****" + vtMa + "****");
+			VatTuDAO VatTuDAO = new VatTuDAO();
+			VatTu vt = VatTuDAO.getVatTu(vtMa);
+			return JSonUtil.toJson(vt);
+		}
+	@RequestMapping(value="/addVattu", method=RequestMethod.GET, 
+			produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	 public @ResponseBody String addVattu(@RequestParam("vtMa") String vtMa, @RequestParam("vtTen") String vtTen, @RequestParam("dvt") String dvt) {
+		String result = "";
+		System.out.println("MA: "+vtMa);
+		if(new VatTuDAO().getVatTu(vtMa) == null)
+		{
+			new VatTuDAO().addVatTu(new VatTu(vtMa, vtTen, dvt));
+			System.out.println("success");
+			result = "success";
+			
+			
+		}
+		else
+		{
+			System.out.println("fail");
+			result = "fail";
+		}
+			return JSonUtil.toJson(result);
+	}
+	@RequestMapping(value="/updateVattu", method=RequestMethod.GET, 
+		produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	 public @ResponseBody String updateVattu(@RequestParam("vtMaUpdate") String vtMaUpdate, @RequestParam("vtTenUpdate") String vtTenUpdate, @RequestParam("dvtUpdate") String dvtUpdate) {
+
+		VatTu vt = new VatTu(vtMaUpdate, vtTenUpdate, dvtUpdate);
+		new VatTuDAO().updateVatTu(vt);
+		return JSonUtil.toJson(vt);
+	}
+	@RequestMapping(value="/deleteVattu", method=RequestMethod.GET, 
+			produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	 public @ResponseBody String deleteVattu(@RequestParam("vtMa") String vtMa) {
+		VatTu vt = new VatTu(vtMa);
+		new VatTuDAO().deleteVatTu(vt);
+		return JSonUtil.toJson(vtMa);
+	}
+}
